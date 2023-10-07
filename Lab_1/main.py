c6064f6c4c44f3c951cd2b6b7abc5e1f2505c018
Sanya1703 <135603523+Alexnader1703@users.noreply.github.com>
2023-10-08 01:03:24 +0300

    Add time counting

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,5 +1,8 @@
 from MergeSort import merge_sort
-
+import time
+start=time.time()
 nums=[0,5,1,6,2,1,6,5,2,1,43,123,4265,12,1345,124,45]
 merge_sort(nums)
+time_job=(time.time() - start)
+print(f"--- {time_job} seconds ---")
 print(nums)

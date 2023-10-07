ead8731e975d56458872c85657162bb6c91f8c6b
Sanya1703 <135603523+Alexnader1703@users.noreply.github.com>
2023-10-08 01:55:20 +0300

    Add a record of the result to a file

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -3,8 +3,12 @@ from MergeSort import merge_sort
 import time
 file=open('Input.txt','r')
 nums= [int(i) for i in file.readline().split(',')]
+file.close()
+
 start=time.time()
 merge_sort(nums)
 time_job=(time.time() - start)
-print(f"--- {time_job} seconds ---")
-print(nums)
+
+file =open('Result.txt','w')
+file.write(str(nums))
+print(f"{len(nums)} numbers per ---{time_job} seconds---")

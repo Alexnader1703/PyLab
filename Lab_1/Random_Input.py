8a2f45f47cdc5d612bff859993f666f7be97d2b7
Sanya1703 <135603523+Alexnader1703@users.noreply.github.com>
2023-10-09 21:28:25 +0300

    Add Func Random Input

--- Lab_1/Random_Input.py
+++ Lab_1/Random_Input.py
@@ -1,9 +1,11 @@
 import random
-n=int(input("Кол-во чисел: "))
+def R_Input():
+    n = int(input("Кол-во чисел: "))
 
-s=""
-for i in (range(n)):
-    s+=str(random.randint(-10000,10000))+','
+    s = ""
+    for i in (range(n)):
+        s += str(random.randint(-10000, 10000)) + ','
 
-file =open('Input.txt','w')
-file.write(str(s[:-1]))
+    file = open('Input.txt', 'w')
+    file.write(str(s[:-1]))
+R_Input()

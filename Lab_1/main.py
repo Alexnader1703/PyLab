e79e41f5c71b7febf1f85fb2e9add8d0598e6642
Sanya1703 <135603523+Alexnader1703@users.noreply.github.com>
2023-10-14 13:59:24 +0300

    Add comments to lab1

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,14 +1,14 @@
 from MergeSort import merge_sort
 
 import time
-file=open('Input.txt','r')
-nums= [int(i) for i in file.readline().split(',')]
+file = open('Input.txt', 'r')
+nums = [int(i) for i in file.readline().split(',')]  # Считываем числа из файла и разбиваем их по запятым
 file.close()
 
-start=time.time()
-merge_sort(nums)
-time_job=(time.time() - start)
+start = time.time()  # Засекаем время перед выполнением сортировки
+merge_sort(nums)  # Вызываем функцию сортировки слиянием
+time_job = (time.time() - start)  # Засекаем время после выполнения сортировки
 
-file =open('Result.txt','w')
-file.write(str(nums))
-print(f"{len(nums)} numbers per {time_job} seconds")
+file = open('Result.txt', 'w')
+file.write(str(nums))  # Записываем отсортированный список в файл
+print(f"{len(nums)} numbers per {time_job} seconds")  # Выводим количество чисел и время сортировки

5092e7ba7872a07bd45aed28f73b3699d3039260
Sanya1703 <135603523+Alexnader1703@users.noreply.github.com>
2023-10-14 13:54:57 +0300

    Add comments

--- Lab_2/Class_Warrior.py
+++ Lab_2/Class_Warrior.py
@@ -1,15 +1,18 @@
 
 class Warrior:
     def __init__(self, name):
+        # Конструктор класса Warrior, инициализирует объект воина с заданным именем.
         self.name = name
-        self.health = 100
-        self.damage = 20
+        self.health = 100  # Устанавливаем начальное здоровье в 100 очков.
+        self.damage = 20   # Устанавливаем урон, который наносит воин при атаке, в 20 очков.
 
     def attack(self, enemy):
-        damage = self.damage
-        enemy.health -= damage
+        # Метод attack позволяет воину атаковать другого воина (передается как аргумент enemy).
+        damage = self.damage  # Определяем урон воина.
+        enemy.health -= damage  # Уменьшаем здоровье врага на величину damage.
         print(f"{self.name} атакует {enemy.name} и наносит {damage} урона.")
         print(f"{enemy.name} осталось {enemy.health} здоровья.")
 
     def is_alive(self):
+        # Метод is_alive проверяет, жив ли воин, основываясь на его текущем здоровье.
         return self.health > 0
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,14 +1,15 @@
 from Class_Warrior import Warrior
 import random
-hero1 = Warrior("Воин 1")
-hero2 = Warrior("Воин 2")
+hero1 = Warrior("Воин 1")  # Создаем первого воина.
+hero2 = Warrior("Воин 2")  # Создаем второго воина.
 
 while hero1.is_alive() and hero2.is_alive():
-    attacker = random.choice([hero1, hero2])
-    enemy = hero2 if attacker == hero1 else hero1
-    attacker.attack(enemy)
+    # Запускаем цикл, который будет продолжаться, пока оба воина живы.
+    attacker = random.choice([hero1, hero2])  # Случайным образом выбираем, кто атакует.
+    enemy = hero2 if attacker == hero1 else hero1  # Определяем, кто является врагом атакующего.
+    attacker.attack(enemy)  # Вызываем метод атаки для атакующего воина.
 
 if hero1.is_alive():
-    print(f"{hero1.name} победил!")
+    print(f"{hero1.name} победил!")  # Если первый воин жив, объявляем его победителем.
 else:
-    print(f"{hero2.name} победил!")
+    print(f"{hero2.name} победил!")  # Иначе объявляем второго воина победителем.
